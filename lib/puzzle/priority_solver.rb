0743349a2333ac925fb76f7b8b82fca858b8ef46
Jon-Michael Deldin <dev@jmdeldin.com>
2012-09-11 22:12:02 -0600

    Refactor solvers to common run() method

---
+++ lib/puzzle/priority_solver.rb
@@ -0,0 +1,43 @@
+require_relative 'solver'
+require 'algorithms'
+
+class Puzzle::PrioritySolver < Puzzle::Solver
+  def initialize(problem, priority_func)
+    @counts = 0
+    @solution = nil
+    @priority_func = priority_func
+    run(problem)
+    compute_path
+  end
+
+  def run(problem)
+    g = Puzzle::Node.new(problem)
+    frontier = Containers::PriorityQueue.new do |x, y|
+      (y <=> x) == 1 # prefer smaller elts
+    end
+
+    frontier.push(g, @priority_func.call(g))
+    marked = {g.board => true}
+
+    until frontier.empty?
+      ng = frontier.pop
+
+      return @solution = ng if ng.problem.solved?
+      @counts += 1
+      progress_report
+
+      # for every action, create some separate graphs
+      ng.problem.actions.each do |action|
+        new_problem = ng.problem.clone
+        new_problem.move(action)
+        new_graph = Puzzle::Node.new(new_problem, ng)
+        new_graph.move = action
+
+        if !marked[new_graph.board]
+          marked[new_graph.board] = true
+          frontier.push(new_graph, @priority_func.call(new_graph))
+        end
+      end
+    end
+  end
+end
--- lib/puzzle/ucs_solver.rb
+++ lib/puzzle/ucs_solver.rb
@@ -1,35 +1,8 @@
-require_relative 'solver'
-require 'algorithms'
+require_relative 'priority_solver'
 
-class Puzzle::UcsSolver < Puzzle::Solver
-  def run(problem)
-    g = Puzzle::Node.new(problem)
-    frontier = Containers::PriorityQueue.new do |x, y|
-      (y <=> x) == 1 # prefer smaller elts
-    end
-
-    frontier.push(g, g.cost)
-    marked = {g.board => true}
-
-    until frontier.empty?
-      ng = frontier.pop
-
-      return @solution = ng if ng.problem.solved?
-      @counts += 1
-      progress_report
-
-      # for every action, create some separate graphs
-      ng.problem.actions.each do |action|
-        new_problem = ng.problem.clone
-        new_problem.move(action)
-        new_graph = Puzzle::Node.new(new_problem, ng)
-        new_graph.move = action
-
-        if !marked[new_graph.board]
-          marked[new_graph.board] = true
-          frontier.push(new_graph, new_graph.cost)
-        end
-      end
-    end
+class Puzzle::UcsSolver < Puzzle::PrioritySolver
+  def initialize(problem)
+    priority = lambda { |graph| graph.cost }
+    super(problem, priority)
   end
 end
