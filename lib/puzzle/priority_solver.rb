d9e306f2993f84971dcbb900c9c881b9e031d1ce
Jon-Michael Deldin <dev@jmdeldin.com>
2012-09-11 23:31:17 -0600

    Add closed set

--- lib/puzzle/priority_solver.rb
+++ lib/puzzle/priority_solver.rb
@@ -19,6 +19,7 @@ class Puzzle::PrioritySolver < Puzzle::Solver
     frontier.push(g, @priority_func.call(g))
     marked = Set.new
     marked.add g.board
+    explored = Set.new
 
     until frontier.empty?
       ng = frontier.pop
@@ -27,12 +28,18 @@ class Puzzle::PrioritySolver < Puzzle::Solver
       @counts += 1
       progress_report
 
+      explored << ng.board
+
       # for every action, create some separate graphs
       ng.problem.actions.each do |action|
         new_problem = ng.problem.clone
         new_problem.move(action)
         new_graph = Puzzle::Node.new(new_problem, ng)
         new_graph.move = action
+
+        if explored.include? new_graph.board
+          next
+        end
 
         unless marked.include? new_graph.board
           marked.add new_graph.board
