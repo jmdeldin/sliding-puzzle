a606faa0435ca290d6feb016898860daa61d579a
Jon-Michael Deldin <dev@jmdeldin.com>
2012-09-11 23:31:08 -0600

    1.8 compat

--- lib/puzzle.rb
+++ lib/puzzle.rb
@@ -1,2 +1,21 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 module Puzzle
+  [:Reader, :Node, :Solver, :Problem].each do |m|
+    autoload m, "puzzle/#{m.to_s.downcase}"
+  end
+  autoload :PrioritySolver, 'puzzle/priority_solver'
+  autoload :AstarSolver, 'puzzle/astar_solver'
+  autoload :BfsSolver, 'puzzle/bfs_solver'
+  autoload :DfsSolver, 'puzzle/dfs_solver'
+  autoload :UcsSolver, 'puzzle/ucs_solver'
+  autoload :GreedySolver, 'puzzle/greedy_solver'
+
 end
